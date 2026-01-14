5295a79a2b0b5e19f67ee866f11cc59050c68b35
Javier <alexguerrero250890@gmail.com>
2026-01-14 13:47:58 +0000

    Add ALB module - prepare dev-stable V1.1

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "this" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.subnet_ids
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+}
+
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
+
+# ✅ USAR COUNT EN VEZ DE for_each
+resource "aws_lb_target_group_attachment" "this" {
+  count = length(var.instance_ids)
+
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = var.instance_ids[count.index]
+  port             = 80
+}
+
---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,5 @@
+variable "project_name" { type = string }
+variable "vpc_id"       { type = string }
+variable "subnet_ids"   { type = list(string) }
+variable "instance_ids" { type = list(string) }
+
